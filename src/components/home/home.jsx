066509b94791964178e20fb9e84253a1c9bb4ccf
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-11 03:20:11 +0900

    feat: 테스트 틀 완료
    결과 mbti 그룹핑하고, 결과내용 노션에 맞게 수정해야함.

---
+++ src/components/home/home.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../home/home.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCopy,
+    faArrowAltCircleRight,
+} from '@fortawesome/free-solid-svg-icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+const Home = () => {
+    const url = window.location.href; // url 복사
+    const copyAlert = () => {
+        alert('링크 생성!');
+    };
+    return (
+        <>
+            <div className={styles.wrapper}>
+                <div className={styles.container}>
+                    <div className={styles.top}>
+                        <img
+                            className={styles.logo}
+                            src="img/salmal.png"
+                            alt="로고"
+                        />
+                    </div>
+                    <div className={styles.middle}>
+                        <h2 className={styles.header}>올해는 어디로 갈까?</h2>
+                        <p>황현정은</p>
+                        <p>집갈래요.</p>
+                    </div>
+                    <div className={styles.bottom}>
+                        <Link to="/tripMBTI" className={styles.start__button}>
+                            테스트 하기
+                            <FontAwesomeIcon
+                                icon={faArrowAltCircleRight}
+                                className={styles.icon}
+                            />
+                        </Link>
+                        <CopyToClipboard text={url}>
+                            <button
+                                className={styles.start__button}
+                                onClick={copyAlert}
+                            >
+                                링크복사
+                                <FontAwesomeIcon
+                                    icon={faCopy}
+                                    className={styles.icon}
+                                />
+                            </button>
+                        </CopyToClipboard>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Home;
