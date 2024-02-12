d79898d09c3256f8363ab09d721d589e26fbb67c
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-13 04:58:02 +0900

    feat: 온보딩 페이지 생성 완

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from '../src/components/home/home';
-import Countries from '../src/components/countries/countries';
-import GlobalStyle from './common/css/globalStyle';
-import Options from './components/options/option';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "../src/components/home/home";
+import Countries from "../src/components/countries/countries";
+import GlobalStyle from "./common/css/globalStyle";
+import Options from "./components/options/option";
+import OnBoarding from "./components/onbording/onboarding";
 const App = () => {
     return (
         <BrowserRouter>
@@ -11,12 +12,15 @@ const App = () => {
                 <Route exact path="/">
                     <Home />
                 </Route>
+                <Route exact path="/onboarding">
+                    {" "}
+                    {/* 새로운 경로로 OnBoarding 페이지를 추가합니다. */}
+                    <OnBoarding />
+                </Route>
                 <Route exact path="/tripMBTI">
                     <Options />
                 </Route>
-                <Route>
-                    <Route path="/result/:countryName" component={Countries} />
-                </Route>
+                <Route path="/result/:countryName" component={Countries} />
             </Switch>
         </BrowserRouter>
     );
