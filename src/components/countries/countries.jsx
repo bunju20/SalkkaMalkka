8f3ac97dfd451ea6ea46384bd0060ad004c1beb0
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-12 18:41:36 +0900

    feat: 결과화면 완전 구현 완료
    INTJ만 기준으로 한거라 나머지 json수정해야함.

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -8,20 +8,32 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import KakaoBtn from "../shareSNS/kakaoShareButton";
 import FacebookBtn from "../shareSNS/facebookShareButton";
 import TwitterBtn from "../shareSNS/twitterShareButton";
+import palaette from "../../lib/styles/colorPalatte.js";
 
 const Profile = ({ match }) => {
     const url = window.location.href;
     const { countryName } = match.params;
     const nation = Countries[countryName];
+    let mainColor;
+    let colorLight;
 
     if (!nation) {
         return <div>존재하지 않는 결과입니다.</div>;
+    } else {
+        // eslint-disable-next-line default-case
+        switch (nation.subhead) {
+            case "MacBook Pro":
+                mainColor = palaette.MacBookPro;
+                colorLight = palaette.MacBookPro_ligth;
+                console.log();
+                break;
+        }
     }
     const copyAlert = () => {
         alert("링크 복사완료!");
     };
     return (
-        <>
+        <div className={styles.wrap__all}>
             <div className={styles.wrapper} key={nation.id}>
                 <div className={styles.container}>
                     <div className={styles.header}></div>
@@ -31,20 +43,39 @@ const Profile = ({ match }) => {
                         className={styles.main__img}
                     />
                     <div className={styles.result__type}>
-                        <h1 className={styles.result__city}>
+                        <h1
+                            className={styles.result__city}
+                            style={{ color: mainColor }}
+                        >
                             {nation.subhead}
                         </h1>
-                        <h1 className={styles.result__city}>
+                        <h1
+                            className={styles.result__device_detail}
+                            style={{ color: mainColor }}
+                        >
                             {nation.subject}
                         </h1>
                         <br />
                     </div>
                     <div className={styles.tagsContainer}>
-                        <div className={styles.tag}>
-                            <span>{nation.mbtitag}</span>
+                        <div
+                            className={styles.tag}
+                            style={{ backgroundColor: colorLight }}
+                        >
+                            <span style={{ color: mainColor }}>
+                                {nation.mbtitag}
+                            </span>
                         </div>
-                        <div className={styles.tag} style={{ width: "60%" }}>
-                            <span>{nation.hashtag}</span>
+                        <div
+                            className={styles.tag}
+                            style={{
+                                width: "226px",
+                                backgroundColor: colorLight,
+                            }}
+                        >
+                            <span style={{ color: mainColor }}>
+                                {nation.hashtag}
+                            </span>
                         </div>
                     </div>
 
@@ -64,7 +95,7 @@ const Profile = ({ match }) => {
                     </div>
                     <div className={styles.result__advice__box}>
                         <div className={styles.result__advice}>
-                            <h4>환상의 조합</h4>
+                            <h4 style={{ color: mainColor }}>환상의 조합</h4>
                             <img
                                 src={nation.duo[0].img}
                                 alt="mbti캐릭터"
@@ -82,7 +113,12 @@ const Profile = ({ match }) => {
                             <p>{nation.counter[0].subhead}</p>
                         </div>
                     </div>
-                    <h1 className={styles.result__city}>테스트결과 공유하기</h1>
+                    <h1
+                        className={styles.result__share}
+                        style={{ color: mainColor }}
+                    >
+                        테스트결과 공유하기
+                    </h1>
                     <div className={styles.shareBox}>
                         <KakaoBtn />
                         <FacebookBtn />
@@ -90,7 +126,7 @@ const Profile = ({ match }) => {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
---
+++ src/lib/styles/colorPalatte.js
@@ -0,0 +1,21 @@
+const palaette = {
+    AirPods:'#FF7A9A',
+    AirPods_light:'#FFE0E8',
+    iPhone:'#12AAFF',
+    iPhone_light:'#CBECFF',
+    iPad:'#0253A3',
+    iPad_light:'#C7E3FF',
+    iMac:'#BD7AFF',
+    iMac_light:'#EBD6FF',
+    MacBookAir:'#FFA013',
+    MacBookAir_light:'#FFF9D7',
+    MacBookPro:'#682102',
+    MacBookPro_ligth:'#F0E6E6',
+    AppleWatch:'#FF3B30',
+    AppleWatch_light:'#FFD4D1',
+    MacMini:"#0C834A",
+    MacMini_ligth:"#B2FFDF",
+
+}
+
+export default palaette;
