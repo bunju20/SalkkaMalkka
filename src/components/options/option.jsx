51eea4e4d55e513b8f6056ae7898e1f4d5f827e1
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-12 18:41:36 +0900

    feat: 결과화면 완전 구현 완료
    INTJ만 기준으로 한거라 나머지 json수정해야함.

--- src/components/options/option.jsx
+++ src/components/options/option.jsx
@@ -1,21 +1,33 @@
-import React, { createRef, useEffect, useState } from 'react';
-import styles from './option.module.css';
-import { useHistory } from 'react-router-dom';
-import Questions from '../../common/api/questionsApi';
+import React, { createRef, useEffect, useState } from "react";
+import styles from "./option.module.css";
+import { useHistory } from "react-router-dom";
+import Questions from "../../common/api/questionsApi";
 
 const Options = () => {
     const [loading, setLoading] = useState(false);
     const [num, setNum] = useState(0);
     const [currentSlide, setCurrentSlide] = useState(1);
     const slideRef = createRef();
-    const TOTAL_SLIDES = Questions.length; 
+    const TOTAL_SLIDES = Questions.length;
     const history = useHistory();
-    const [mbtiCounts, setMbtiCounts] = useState({I: 0, E: 0, S: 0, N: 0, T: 0, F: 0, J: 0, P: 0});
+    const [mbtiCounts, setMbtiCounts] = useState({
+        I: 0,
+        E: 0,
+        S: 0,
+        N: 0,
+        T: 0,
+        F: 0,
+        J: 0,
+        P: 0,
+    });
     const [finalMbti, setFinalMbti] = useState("");
 
     const updateMbtiCountAndNavigate = (type) => {
-        setMbtiCounts(prevCounts => {
-            const updatedCounts = { ...prevCounts, [type]: prevCounts[type] + 1 };
+        setMbtiCounts((prevCounts) => {
+            const updatedCounts = {
+                ...prevCounts,
+                [type]: prevCounts[type] + 1,
+            };
 
             if ((num + 1) % 3 === 0 || currentSlide === TOTAL_SLIDES) {
                 const mbtiResult = calculateMbtiResult(updatedCounts);
@@ -32,11 +44,15 @@ const Options = () => {
     };
 
     const calculateMbtiResult = (counts) => {
-        const mbtiPairs = ['EI', 'SN', 'TF', 'JP'];
-        return mbtiPairs.map(pair => {
-            const [firstType, secondType] = pair.split('');
-            return counts[firstType] >= counts[secondType] ? firstType : secondType;
-        }).join('');
+        const mbtiPairs = ["EI", "SN", "TF", "JP"];
+        return mbtiPairs
+            .map((pair) => {
+                const [firstType, secondType] = pair.split("");
+                return counts[firstType] >= counts[secondType]
+                    ? firstType
+                    : secondType;
+            })
+            .join("");
     };
 
     const nextSlide = (answerIndex) => {
@@ -46,13 +62,12 @@ const Options = () => {
         setNum(num + 1);
         setCurrentSlide(currentSlide + 1);
         if (slideRef.current) {
-            slideRef.current.style.transform += 'translateX(-100vw)';
+            slideRef.current.style.transform += "translateX(-100vw)";
         }
     };
 
     useEffect(() => {
         if (!loading && currentSlide > TOTAL_SLIDES) {
-            
             setLoading(true);
             setTimeout(() => {
                 history.push(`/result/${finalMbti}`);
@@ -70,14 +85,32 @@ const Options = () => {
                                 {/* Question and Answer Rendering */}
                                 <div className={styles.top}>
                                     <div className={styles.mbti__counter}>
-                                        <span className={styles.mbti__progress__color}>{currentSlide}</span>
-                                        <span className={styles.mbti__end__color}>/{TOTAL_SLIDES}</span>
+                                        <span
+                                            className={
+                                                styles.mbti__progress__color
+                                            }
+                                        >
+                                            {currentSlide}
+                                        </span>
+                                        <span
+                                            className={styles.mbti__end__color}
+                                        >
+                                            /{TOTAL_SLIDES}
+                                        </span>
                                     </div>
-                                    <h1 className={styles.mbti__question}>{item.question}</h1>
+                                    <h1 className={styles.mbti__question}>
+                                        {item.question}
+                                    </h1>
                                 </div>
                                 <article className={styles.mbti__btn__box}>
                                     {item.answers.map((answer, answerIndex) => (
-                                        <button key={answer.type} className={styles.mbti__button} onClick={() => nextSlide(answerIndex)}>
+                                        <button
+                                            key={answer.type}
+                                            className={styles.mbti__button}
+                                            onClick={() =>
+                                                nextSlide(answerIndex)
+                                            }
+                                        >
                                             {answer.content}
                                         </button>
                                     ))}
