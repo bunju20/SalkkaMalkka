ab6287764f0b0a51f10f532cb4a3c1cf346f9dcc
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-12 14:32:15 +0900

    feat: INTJ json 확정, 결과화면 일부 구현
    json 고쳐야하고 결과화면 폰트 및 색상 수정해야함.

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,13 +1,13 @@
-import styles from './countries.module.css';
-import { Link } from 'react-router-dom';
-import Countries from '../../common/api/countriesApi/../countriesApi';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignOutAlt, faCopy } from '@fortawesome/free-solid-svg-icons';
-import { faEarlybirds } from '@fortawesome/free-brands-svg-icons';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import KakaoBtn from '../shareSNS/kakaoShareButton';
-import FacebookBtn from '../shareSNS/facebookShareButton';
-import TwitterBtn from '../shareSNS/twitterShareButton';
+import styles from "./countries.module.css";
+import { Link } from "react-router-dom";
+import Countries from "../../common/api/countriesApi/../countriesApi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt, faCopy } from "@fortawesome/free-solid-svg-icons";
+import { faEarlybirds } from "@fortawesome/free-brands-svg-icons";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import KakaoBtn from "../shareSNS/kakaoShareButton";
+import FacebookBtn from "../shareSNS/facebookShareButton";
+import TwitterBtn from "../shareSNS/twitterShareButton";
 
 const Profile = ({ match }) => {
     const url = window.location.href;
@@ -18,22 +18,13 @@ const Profile = ({ match }) => {
         return <div>존재하지 않는 결과입니다.</div>;
     }
     const copyAlert = () => {
-        alert('링크 복사완료!');
+        alert("링크 복사완료!");
     };
     return (
         <>
             <div className={styles.wrapper} key={nation.id}>
                 <div className={styles.container}>
-                    <div className={styles.header}>
-                        <>
-                            <Link to="/">
-                                <FontAwesomeIcon
-                                    icon={faEarlybirds}
-                                    className={styles.birds}
-                                />
-                            </Link>
-                        </>
-                    </div>
+                    <div className={styles.header}></div>
                     <img
                         src={nation.img}
                         alt="img"
@@ -41,86 +32,61 @@ const Profile = ({ match }) => {
                     />
                     <div className={styles.result__type}>
                         <h1 className={styles.result__city}>
+                            {nation.subhead}
+                        </h1>
+                        <h1 className={styles.result__city}>
                             {nation.subject}
                         </h1>
                         <br />
                     </div>
-                    <div className={styles.reust__title}>
-                        <h2>{nation.id}의 여행특징은?</h2>
+                    <div className={styles.tagsContainer}>
+                        <div className={styles.tag}>
+                            <span>{nation.mbtitag}</span>
+                        </div>
+                        <div className={styles.tag} style={{ width: "60%" }}>
+                            <span>{nation.hashtag}</span>
+                        </div>
                     </div>
-                    <ul className={styles.result__style__wrapper}>
-                        {nation.description.map((item) => {
-                            return (
-                                <li
-                                    className={styles.result__style__detail}
-                                    key={item.des}
-                                >
-                                    {item.des}
-                                </li>
-                            );
-                        })}
-                    </ul>
+
+                    <div className={styles.result__list__box}>
+                        <ul className={styles.result__style__wrapper}>
+                            {nation.description.map((item) => {
+                                return (
+                                    <li
+                                        className={styles.result__style__detail}
+                                        key={item.des}
+                                    >
+                                        {item.des}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
                     <div className={styles.result__advice__box}>
                         <div className={styles.result__advice}>
-                            <a href={`${/result/}${nation.duo[0].subhead}`}>
-                                <img
-                                    src={nation.duo[0].img}
-                                    alt="mbti캐릭터"
-                                    Link="/"
-                                />
-                            </a>
-                            <div>
-                                <h4>함께하면 좋아요!</h4>
-                                <p className={styles.advice__strong}>
-                                    찰떡궁합 그 자체 {nation.duo[0].subhead}
-                                </p>
-                                <p>{nation.duo[0].des}</p>
-                            </div>
+                            <h4>환상의 조합</h4>
+                            <img
+                                src={nation.duo[0].img}
+                                alt="mbti캐릭터"
+                                Link="/"
+                            />
+                            <p>{nation.duo[0].subhead}</p>
                         </div>
                         <div className={styles.result__advice}>
-                            <Link
-                                to={`${/result/}${nation.counter[0].subhead}`}
-                            >
-                                <img
-                                    src={nation.counter[0].img}
-                                    alt="mbti캐릭터"
-                                />
-                            </Link>
-
-                            <div>
-                                <h4>가능하면 피하는게 좋겠어요!</h4>
-                                <p className={styles.advice__strong}>
-                                    도망가세요 {nation.counter[0].subhead}
-                                </p>
-                                <p>{nation.counter[0].des}</p>
-                            </div>
+                            <h4>환장의 조합</h4>
+                            <img
+                                src={nation.counter[0].img}
+                                alt="mbti캐릭터"
+                                Link="/"
+                            />
+                            <p>{nation.counter[0].subhead}</p>
                         </div>
                     </div>
+                    <h1 className={styles.result__city}>테스트결과 공유하기</h1>
                     <div className={styles.shareBox}>
                         <KakaoBtn />
                         <FacebookBtn />
                         <TwitterBtn />
-                    </div>
-                    <div className={styles.button__box}>
-                        <Link to="/" className={styles.button}>
-                            다시하기
-                            <FontAwesomeIcon
-                                icon={faSignOutAlt}
-                                className={styles.icon}
-                            />
-                        </Link>
-                        <CopyToClipboard text={url}>
-                            <button
-                                className={styles.copy__button}
-                                onClick={copyAlert}
-                            >
-                                링크복사
-                                <FontAwesomeIcon
-                                    icon={faCopy}
-                                    className={styles.icon}
-                                />
-                            </button>
-                        </CopyToClipboard>
                     </div>
                 </div>
             </div>
