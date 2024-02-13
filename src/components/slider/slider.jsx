7614431c883280c2d39a059a81f22c316c2f8112
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-14 01:26:43 +0900

    feat: 결과화면 만듦
    옵션화면에 프로그래스 바있는데 그거 추가하면 결과화면 완전 완성됨

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import styles from "./slider.module.css";
 import SimpleSlider from "./simpleSlider";
+import { Link } from "react-router-dom";
 
 const SliderScreen = () => {
     return (
         <div className={styles.wrap__all}>
             <div className={styles.wrapper}>
                 <div className={styles.container}>
-                    <SimpleSlider className={styles.slider__slider} />
+                    <h1 className={styles.result__city}>테스트 완료!</h1>
+                    <SimpleSlider />
+                    <Link to="/onboarding" className={styles.start__button}>
+                        <div className={styles.button__wrap}>
+                            <p className={styles.second}>
+                                쿠팡 방문하고 환생결과 확인하기
+                            </p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
