65e71f942644f744aa638375d26443b50d3e4556
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-14 01:26:43 +0900

    feat: 결과화면 만듦
    옵션화면에 프로그래스 바있는데 그거 추가하면 결과화면 완전 완성됨

--- src/components/slider/simpleSlider.jsx
+++ src/components/slider/simpleSlider.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styles from "./slider.module.css";
 
 export default function SimpleSlider() {
     var settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 700,
         slidesToShow: 1,
@@ -18,28 +19,60 @@ export default function SimpleSlider() {
     return (
         <Slider {...settings}>
             <div>
-                <img src="/img/card/AirPod.svg" alt="airpod" />
+                <img
+                    src="/img/card/Group 341.svg"
+                    alt="Group 341"
+                    className={styles.img__card}
+                />
             </div>
             <div>
-                <img src="/img/card/appleWatch.svg" alt="" />
+                <img
+                    src="/img/card/Group 342.svg"
+                    alt="Group 342"
+                    className={styles.img__card}
+                />
             </div>
             <div>
-                <img src="/img/card/iMac.svg" alt="" />
+                <img
+                    src="/img/card/Group 343.svg"
+                    alt="Group 343"
+                    className={styles.img__card}
+                />
             </div>
             <div>
-                <img src="/img/card/iPad.svg" alt="" />
+                <img
+                    src="/img/card/Group 344.svg"
+                    alt="Group 344"
+                    className={styles.img__card}
+                />
             </div>
             <div>
-                <img src="/img/card/iphonesvg.svg" alt="" />
+                <img
+                    src="/img/card/Group 345.svg"
+                    alt="Group 345"
+                    className={styles.img__card}
+                />
             </div>
             <div>
-                <img src="/img/card/macbookAir.svg" alt="" />
+                <img
+                    src="/img/card/Group 346.svg"
+                    alt="Group 346"
+                    className={styles.img__card}
+                />
             </div>
             <div>
-                <img src="/img/card/macbookPro.svg" alt="" />
+                <img
+                    src="/img/card/Group 347.svg"
+                    alt="Group 347"
+                    className={styles.img__card}
+                />
             </div>
             <div>
-                <img src="/img/card/macMini.svg" alt="" />
+                <img
+                    src="/img/card/Group 348.svg"
+                    alt="Group 348"
+                    className={styles.img__card}
+                />
             </div>
         </Slider>
     );
