cb6004db51505b73cde8c9f1da7bacf005495382
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-14 02:43:24 +0900

    feat: 마지막 질문, 슬라이드 , 결과화면 순서대로 이동하도록 완료

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import styles from "./slider.module.css";
 import SimpleSlider from "./simpleSlider";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const SliderScreen = () => {
+    const location = useLocation();
+    const finalMbti = location.state.finalMbti;
+
     return (
         <div className={styles.wrap__all}>
             <div className={styles.wrapper}>
                 <div className={styles.container}>
                     <h1 className={styles.result__city}>테스트 완료!</h1>
                     <SimpleSlider />
-                    <Link to="/onboarding" className={styles.start__button}>
+                    <Link
+                        to={`/result/${finalMbti}`}
+                        className={styles.start__button}
+                    >
                         <div className={styles.button__wrap}>
                             <p className={styles.second}>
                                 쿠팡 방문하고 환생결과 확인하기
