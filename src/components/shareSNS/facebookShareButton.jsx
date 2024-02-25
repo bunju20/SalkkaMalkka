7079f658c6a3604ca945bb48e26b0a1c0a98768f
bunju20 <85238126+bunju20@users.noreply.github.com>
2024-02-26 01:07:32 +0900

    feat: 링크 복사하기 구현 완

--- src/components/shareSNS/facebookShareButton.jsx
+++ src/components/shareSNS/facebookShareButton.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import styles from "./shareIcon.module.css";
+
 const FacebookShareButton = () => {
-    const shareFacebook = () => {
-        window.open(
-            "http://www.facebook.com/sharer.php?u=https://felizmbti.netlify.app/"
-        );
+    const copyToClipboard = () => {
+        // 현재 페이지의 URL을 클립보드에 복사합니다.
+        navigator.clipboard
+            .writeText(window.location.href)
+            .then(() => {
+                // 복사 성공 시 로그나 사용자에게 알림을 표시할 수 있습니다.
+                console.log("URL copied to clipboard successfully!");
+                alert("URL has been copied to clipboard!");
+            })
+            .catch((err) => {
+                // 복사 실패 시 에러 처리를 할 수 있습니다.
+                console.error("Failed to copy URL: ", err);
+                alert("Failed to copy URL");
+            });
     };
     return (
-        <div>
-            <a href="/#" onClick={shareFacebook}>
-                <img
-                    src="/img/result/circle_insta.jpg"
-                    className={styles.facebook__icon}
-                    alt="facebook_icon"
-                />
-            </a>
+        <div onClick={copyToClipboard}>
+            <img
+                src="/img/clip.svg"
+                className={styles.facebook__icon}
+                alt="facebook_icon"
+            />
         </div>
     );
 };
